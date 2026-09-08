cbb08b89396a0b48aa6f307f2c9f34c31db70f57
YiJie <yijie4188@gmail.com>
2026-09-09 06:35:04 +0800

    feat(host): render bounded declarative game scenes

---
+++ packages/cli/src/renderer/restricted-content/index.ts
@@ -0,0 +1,9 @@
+export { encodeRestrictedJson, RESTRICTED_LIMITS, type RestrictedJson } from './json.js'
+export { type RestrictedScene, type RestrictedSceneNode, validateRestrictedScene } from './scene.js'
+export {
+  mountRestrictedScene,
+  type RestrictedSceneActionResult,
+  type RestrictedSceneMountOptions,
+  type RestrictedScenePublishResult,
+  type RestrictedSceneSeat,
+} from './renderer.js'
